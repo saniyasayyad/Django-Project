202613f1917c69dc665bf7b86754f6f4b9f3c174
saniyasayyad <saniyasayyad4717@gmail.com>
2025-03-21 14:57:58 +0530

    After Making th Changes

--- flipkart/electronics/models.py
+++ flipkart/electronics/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=500)
     price = models.IntegerField()
     description = models.TextField()
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):  # Fix indentation here
         return self.name
--- flipkart/electronics/views.py
+++ flipkart/electronics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 from .forms import ProductForm
 # Create your views here.
@@ -11,6 +11,28 @@ def product_detail(request):
     form = ProductForm()
     if request.method == 'POST':
         form = ProductForm(request.POST)
-        if form.isvalid():
+        if form.is_valid():
             form.save()
-    return render(request, 'electronics/product_detail.html', {"prod":prod})
+            form = ProductForm()
+    return render(request, 'electronics/product_detail.html', {"prod":prod, 'form':form})
+
+def delete_product(request, id):
+            prod = get_object_or_404(Product, id=id)
+            prod.delete()
+            return redirect('product_detail')
+
+def update_product(request, id):
+        prod = get_object_or_404(Product, id=id)
+
+        if request.method == 'POST':
+            form = ProductForm(request.POST, instance=prod)
+            if form.is_valid():
+                form.save()
+                return redirect('product_detail')
+        else:
+            form = ProductForm(instance=prod)
+
+        return render(request, 'electronics/update_product.html',{"form":form, "prod":prod})
+
+
+       
